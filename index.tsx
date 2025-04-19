e3ca453019b11b730514e18d677b06268be387b1
slymnoyann <oyansuleymann@gmail.com>
2025-04-19 13:44:00 +0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="w-full h-screen">
+      <iframe
+        src="https://onchaingm.com"
+        title="Onchain GM"
+        className="w-full h-full border-none rounded-2xl"
+      />
+    </div>
+  );
+};
+
+export default Home;
