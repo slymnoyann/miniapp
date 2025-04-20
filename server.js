94026bb7a354cb71caaf21b56b4b764333d891d1
slymnoyann <oyansuleymann@gmail.com>
2025-04-20 15:04:14 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// __dirname simulation (for ESM)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`onchaingm.com is running at http://localhost:${PORT}`);
+});
